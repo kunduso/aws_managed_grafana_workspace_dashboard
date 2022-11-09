aab2e5364506a4315ee52bca1acfd8dfdb683ee5
Sourav Kundu <skundu.dev@gmail.com>
2022-11-09 07:32:46 -0600

    updated config to point to correct state bucket

--- amg_workspace/backend.tf
+++ amg_workspace/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "skundu-terraform-remote-state"
+    bucket  = "terraform-remote-bucket-skundu"
     encrypt = true
     key     = "tf/managed-grafana/workspace/terraform.tfstate"
     region  = "us-east-2"
