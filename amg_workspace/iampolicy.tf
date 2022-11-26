878ed36de29f6ff801b96f7b3ddc3fff602762ee
Sourav Kundu <skundu.dev@gmail.com>
2022-11-26 10:43:33 -0600

    added an iam policy to read cloudwatch logs and metrcs

---
+++ amg_workspace/iampolicy.tf
@@ -0,0 +1,41 @@
+#Create a policy
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+resource "aws_iam_policy" "grafana_cw_policy" {
+  name        = "grafana_cw_policy"
+  path        = "/"
+  description = "Allows Amazon Grafana to access CloudWatch"
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    "Statement": [
+        {
+            "Sid": "AllowReadingMetricsFromCloudWatch",
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:DescribeAlarmsForMetric",
+                "cloudwatch:DescribeAlarmHistory",
+                "cloudwatch:DescribeAlarms",
+                "cloudwatch:ListMetrics",
+                "cloudwatch:GetMetricStatistics",
+                "cloudwatch:GetMetricData",
+                "cloudwatch:GetInsightRuleReport"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowReadingLogsFromCloudWatch",
+            "Effect": "Allow",
+            "Action": [
+                "logs:DescribeLogGroups",
+                "logs:GetLogGroupFields",
+                "logs:StartQuery",
+                "logs:StopQuery",
+                "logs:GetQueryResults",
+                "logs:GetLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+  })
+}
