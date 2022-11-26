5316dec1e8bf16f1f9c64855611ba22599e4c58a
Sourav Kundu <skundu.dev@gmail.com>
2022-11-26 06:01:13 -0600

    added dashboard code

--- amg_dashboard/main.tf
+++ amg_dashboard/main.tf
@@ -1,3 +1,9 @@
+https://registry.terraform.io/providers/grafana/grafana/latest/docs/resources/folder
 resource "grafana_folder" "data" {
   title = "data"
+}
+#https://registry.terraform.io/providers/grafana/grafana/latest/docs/resources/dashboard
+resource "grafana_dashboard" "metrics" {
+  config_json = file("/dashboard/aws-lambda_rev13.json")
+  folder = grafana_folder.data.id
 }
