6d30f53080827a88683849f60cf91d93619a3bd3
Sourav Kundu <skundu.dev@gmail.com>
2022-11-26 06:13:51 -0600

    added a data source

--- amg_dashboard/main.tf
+++ amg_dashboard/main.tf
@@ -1,3 +1,8 @@
+#https://registry.terraform.io/providers/grafana/grafana/latest/docs/resources/data_source
+resource "grafana_data_source" "cloudwatch" {
+  type = "cloudwatch"
+  name = "cw-datasource"
+}
 #https://registry.terraform.io/providers/grafana/grafana/latest/docs/resources/folder
 resource "grafana_folder" "data" {
   title = "data"
